6eb71401bb757d40ca904855fdc240e11ecca18f
Zhang Ziqing <zhangziqing9926@gmail.com>
2022-01-30 17:06:57 +0800

    Add frontend error handling for invalid time ranges
    If the date is invalid, the `resolveLocalDateTime` method returns NaN instead of a proper number.
    This NaN is then passed to the backend as an invalid value,
    and so the error is eventually thrown by the backend with unclear error messages.
    
    Let's
    * raise the error in frontend for invalid date range before it is passed
      to backend.
    * handle the error using try-catch blocks in frontend as well.

--- src/web/app/pages-instructor/instructor-session-edit-page/instructor-session-edit-page.component.ts
+++ src/web/app/pages-instructor/instructor-session-edit-page/instructor-session-edit-page.component.ts
@@ -403,38 +403,43 @@ export class InstructorSessionEditPageComponent extends InstructorSessionBasePag
     this.sessionEditFormModel.isEditable = false;
     this.feedbackSessionModelBeforeEditing = JSON.parse(JSON.stringify(this.sessionEditFormModel));
     this.sessionEditFormModel.isSaving = true;
-
-    const submissionStartTime: number = this.timezoneService.resolveLocalDateTime(
+    let currentCheckingTime: string = '';
+    try {
+      currentCheckingTime = 'Submission opening time';
+      const submissionStartTime: number = this.timezoneService.resolveLocalDateTime(
         this.sessionEditFormModel.submissionStartDate, this.sessionEditFormModel.submissionStartTime,
         this.sessionEditFormModel.timeZone, true);
-    const submissionEndTime: number = this.timezoneService.resolveLocalDateTime(
-        this.sessionEditFormModel.submissionEndDate, this.sessionEditFormModel.submissionEndTime,
-        this.sessionEditFormModel.timeZone, true);
-    let sessionVisibleTime: number = 0;
-    if (this.sessionEditFormModel.sessionVisibleSetting === SessionVisibleSetting.CUSTOM) {
-      sessionVisibleTime = this.timezoneService.resolveLocalDateTime(
-          this.sessionEditFormModel.customSessionVisibleDate, this.sessionEditFormModel.customSessionVisibleTime,
+      currentCheckingTime = 'Submission closing time';
+      const submissionEndTime: number = this.timezoneService.resolveLocalDateTime(
+          this.sessionEditFormModel.submissionEndDate, this.sessionEditFormModel.submissionEndTime,
           this.sessionEditFormModel.timeZone, true);
-    }
-    let responseVisibleTime: number = 0;
-    if (this.sessionEditFormModel.responseVisibleSetting === ResponseVisibleSetting.CUSTOM) {
-      responseVisibleTime = this.timezoneService.resolveLocalDateTime(
-          this.sessionEditFormModel.customResponseVisibleDate, this.sessionEditFormModel.customResponseVisibleTime,
-          this.sessionEditFormModel.timeZone, true);
-    }
+      currentCheckingTime = 'Session visible time';
+      let sessionVisibleTime: number = 0;
+      if (this.sessionEditFormModel.sessionVisibleSetting === SessionVisibleSetting.CUSTOM) {
+        sessionVisibleTime = this.timezoneService.resolveLocalDateTime(
+            this.sessionEditFormModel.customSessionVisibleDate, this.sessionEditFormModel.customSessionVisibleTime,
+            this.sessionEditFormModel.timeZone, true);
+      }
+      currentCheckingTime = 'Response visible time';
+      let responseVisibleTime: number = 0;
+      if (this.sessionEditFormModel.responseVisibleSetting === ResponseVisibleSetting.CUSTOM) {
+        responseVisibleTime = this.timezoneService.resolveLocalDateTime(
+            this.sessionEditFormModel.customResponseVisibleDate, this.sessionEditFormModel.customResponseVisibleTime,
+            this.sessionEditFormModel.timeZone, true);
+      }
 
-    this.feedbackSessionsService.updateFeedbackSession(this.courseId, this.feedbackSessionName, {
-      instructions: this.sessionEditFormModel.instructions,
+      this.feedbackSessionsService.updateFeedbackSession(this.courseId, this.feedbackSessionName, {
+        instructions: this.sessionEditFormModel.instructions,
 
-      submissionStartTimestamp: submissionStartTime,
-      submissionEndTimestamp: submissionEndTime,
-      gracePeriod: this.sessionEditFormModel.gracePeriod,
+        submissionStartTimestamp: submissionStartTime,
+        submissionEndTimestamp: submissionEndTime,
+        gracePeriod: this.sessionEditFormModel.gracePeriod,
 
-      sessionVisibleSetting: this.sessionEditFormModel.sessionVisibleSetting,
-      customSessionVisibleTimestamp: sessionVisibleTime,
+        sessionVisibleSetting: this.sessionEditFormModel.sessionVisibleSetting,
+        customSessionVisibleTimestamp: sessionVisibleTime,
 
-      responseVisibleSetting: this.sessionEditFormModel.responseVisibleSetting,
-      customResponseVisibleTimestamp: responseVisibleTime,
+        responseVisibleSetting: this.sessionEditFormModel.responseVisibleSetting,
+        customResponseVisibleTimestamp: responseVisibleTime,
 
       isClosingEmailEnabled: this.sessionEditFormModel.isClosingEmailEnabled,
       isPublishedEmailEnabled: this.sessionEditFormModel.isPublishedEmailEnabled,
@@ -446,10 +451,16 @@ export class InstructorSessionEditPageComponent extends InstructorSessionBasePag
     })).subscribe((feedbackSession: FeedbackSession) => {
       this.sessionEditFormModel = this.getSessionEditFormModel(feedbackSession);
 
-      this.statusMessageService.showSuccessToast('The feedback session has been updated.');
-    }, (resp: ErrorMessageOutput) => {
-      this.statusMessageService.showErrorToast(resp.error.message);
-    });
+        this.statusMessageService.showSuccessToast('The feedback session has been updated.');
+      }, (resp: ErrorMessageOutput) => {
+        this.statusMessageService.showErrorToast(resp.error.message);
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        this.statusMessageService.showErrorToast(`${error.message} for ${currentCheckingTime}`);
+        this.sessionEditFormModel.isSaving = false;
+      }
+    }
   }
 
   /**
