99eefb73bec94a9432b63c45b61c72560425ea99
Nicolas Chang Weng Yew <nicolas.chang99@gmail.com>
2022-02-02 11:17:19 +0800

    Rename statistics DB to minute statistics DB

--- src/main/java/teammates/storage/api/FeedbackResponseStatisticsMinuteDb.java
+++ src/main/java/teammates/storage/api/FeedbackResponseStatisticsMinuteDb.java
@@ -1,11 +1,48 @@
 package teammates.storage.api;
 
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.LoadType;
+
+import teammates.common.datatransfer.attributes.FeedbackResponseStatisticAttributes;
+import teammates.storage.entity.FeedbackResponse;
+import teammates.storage.entity.FeedbackResponseStatistic;
+
 /**
- * Handles statistics for FeedbackRepsonse.
- *
- * @see FeedbackResponsesDb
+ * Handles statistics at 1 minute intervals for FeedbackResponse.
  */
-public class FeedbackResponseStatisticsMinuteDb {
-/*     public String id;
- */
+public class FeedbackResponseStatisticsMinuteDb extends EntitiesDb<FeedbackResponseStatistic, FeedbackResponseStatisticAttributes> {
+	private static final FeedbackResponseStatisticsMinuteDb instance = new FeedbackResponseStatisticsMinuteDb();
+
+	public static FeedbackResponseStatisticsMinuteDb inst() {
+        return instance;
+    }
+
+	/**
+	 * Checks whether there are existing entities in the database.
+	 */
+	@Override
+	public boolean hasExistingEntities(FeedbackResponseStatisticAttributes feedbackResponseStatistic) {
+		return !load()
+                .filterKey(Key.create(FeedbackResponseStatistic.class,
+                        FeedbackResponse.generateId(entityToCreate.getFeedbackQuestionId(),
+                                entityToCreate.getGiver(), entityToCreate.getRecipient())))
+                .list()
+                .isEmpty();
+	}
+
+	@Override
+	public LoadType<FeedbackResponseStatistic> load() {
+		return ofy().load().type(FeedbackResponseStatistic.class); 
+	}
+	/**
+     * Converts from entity to attributes.
+     */
+	public FeedbackResponseStatisticAttributes makeAttributes(FeedbackResponseStatistic statistic) {
+		assert statistic != null;
+
+        return FeedbackResponseStatisticAttributes.valueOf(statistic);
+	}
+	
 }
